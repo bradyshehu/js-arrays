1904cc1b9e5b2dd2f96eb23617b0752052d9c296
bradyshehu <shehu.brady@gmail.com>
2025-04-03 15:47:24 +0200

    small fix

--- script.js
+++ script.js
@@ -49,5 +49,5 @@ console.log(lewisIndex);
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 
-const isTeachersEmpty = teachers.lenght === 0 ? true : false;
+const isTeachersEmpty = teachers.length === 0 ? true : false;
 console.log(isTeachersEmpty);
